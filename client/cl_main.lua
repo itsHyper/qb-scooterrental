59966192f2bcbfccdfd630cf6e4b27ac5d2e3805
Hyper <rjfance@gmail.com>
2022-03-29 21:44:42 -0400

    Initial commit

---
+++ client/cl_main.lua
@@ -0,0 +1,22 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+    PlayerJob = QBCore.Functions.GetPlayerData().job
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload')
+AddEventHandler('QBCore:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+end)
+
+
+
+
